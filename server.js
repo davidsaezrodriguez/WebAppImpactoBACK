17d0064317a81da5bee262c0fe0aeb8d1705e03c
David <55287732+davidsaezrodriguez@users.noreply.github.com>
2020-05-14 22:13:45 +0200

    AÑADIDO CORS PARA ACCESO DESDE FRONT

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const propiedades = require('./configuracionConexionDB/properties');
 const rutas = require('./scriptsServidor/rutas');
 const DB = require('./configuracionConexionDB/db');
 
+// Inicializamos la app express 
+const app = express();
+
 // Realizamos conexion con base de datos
 DB();
 
-// Inicializamos la app express 
-const app = express();
+// Configuramos cors para que deje acceder a la api a los dominios indicados
+const cors = require('cors');
+app.use(cors(
+    [
+        {
+            origin: "http://localhost:4200/" // Ruta de localhost para que la api nos deje acceder desde el servidor de desarrollo
+        },
+        {
+            origin: "https://webappimpactoback.herokuapp.com/" // Ruta de heroku para que la api nos deje acceder desde el servidor de produccion
+        }
+    ]
+));
 
 // Con body parser transformamos la respuesta post en json y para asi poder coger variable: valor eon rea.body
 app.use(bodyParser.json());
